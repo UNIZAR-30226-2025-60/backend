a0c03417b0afba58b352b55d56cb1c7e830bf73c
Javiersusin <872847@unizar.es>
2025-02-14 19:12:04 +0100

    arreglo apiforo

--- routes/APIforo.js
+++ routes/APIforo.js
@@ -3,11 +3,11 @@ const passport = require("passport");
 
 const router = express.Router();
 
-import { obtenerPreguntas, obtenerPreguntasPorUsuario, obtenerForoCompleto } from '../models/foro';
+const { obtenerPreguntas, obtenerPreguntasPorUsuario, obtenerForoCompleto } = require('../models/foro');
 
 //obtener foro entero
-export default async function handler(req, res) {
-  try {
+async function handler(req, res) {
+    try {
     const foro = await obtenerForoCompleto();
     res.status(200).json(foro);
   } catch (error) {
@@ -18,8 +18,8 @@ export default async function handler(req, res) {
 
 //si le mandamos el cuerpo de un usuario nos devolverá las preguntas que ha hecho ese usuario
 //si no le mandamos nada nos devolverá todas las preguntas
-export const handlerPreguntas = async (req, res) => {
-  const { usuarioCorreo } = req.query;
+async function handlerPreguntas(req, res) {
+    const { usuarioCorreo } = req.query;
   try {
     const preguntas = usuarioCorreo ? await obtenerPreguntasPorUsuario(usuarioCorreo) : await obtenerPreguntas();
     res.status(200).json(preguntas);
