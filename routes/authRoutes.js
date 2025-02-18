35b774d3d4332ee48debbe7140ac1c18ad1f1035
Javiersusin <872847@unizar.es>
2025-02-18 22:37:38 +0100

    ruta authroutes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,10 +6,6 @@ const router = express.Router();
 // Ruta para iniciar sesión con Google
 router.get("/google", passport.authenticate("google", { scope: ["profile", "email"] }));
 
-//si esta usandose desde render se redirige a la pagina de inicio de la aplicacion
-//si no se redirige a localhost:8081/inicio
-const REDIRECT_URL = process.env.RENDER ? 'https://tu-frontend.onrender.com/inicio' : 'http://localhost:8081/inicio';
-
 // Ruta de redirección después de autenticación
 router.get(
   "/google/callback",
@@ -17,7 +13,7 @@ router.get(
   (req, res) => {
     console.log("Usuario autenticado:", req.user); // Verificar usuario autenticado
     console.log("Sesión actual:", req.session); // Imprimir sesión
-    res.redirect(REDIRECT_URL); // Redirigir dinámicamente
+    res.redirect("http://localhost:8081/inicio"); // Redirigir al frontend
   }
 );
 
