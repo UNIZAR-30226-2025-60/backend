4ba135286613ac928bc78a3a443a09a860139988
Javiersusin <872847@unizar.es>
2025-04-04 17:53:08 +0200

    cambios para acceder sin inicio de sesión

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,9 +4,9 @@ const router = express.Router();
 
 // Ruta para obtener los datos del usuario autenticado
 router.get("/user", (req, res) => {
-  if (!req.isAuthenticated()) {
-    return res.status(401).json({ error: "No autenticado" });
-  }
+  // if (!req.isAuthenticated()) {
+  //   return res.status(401).json({ error: "No autenticado" });
+  // }
   res.json(req.user);
 });
 
