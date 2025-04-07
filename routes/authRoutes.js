8a7d268096312c4838ed220d844029be9f22ea7e
Javiersusin <872847@unizar.es>
2025-04-07 19:08:39 +0200

    cierre sesion google

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -42,7 +42,12 @@ router.get(
   (req, res) => {
     console.log("✅ Usuario autenticado con Google:", req.user);
     console.log("📌 Sesión actual:", req.session);
-
+    res.cookie("isGoogleAuth", "true", {
+      httpOnly: false, // Necesario para acceder desde el frontend
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      maxAge: 24 * 60 * 60 * 1000, // 1 día de duración
+    });
     // **Si el usuario está autenticado, guardar su correo en la cookie**
     if (req.user) {
       res.cookie("userEmail", req.user.correo, {
@@ -57,107 +62,4 @@ router.get(
   }
 );
 
-// // Ruta de redirección después de autenticación en móvil
-// router.get(
-//   "/google/callbackM",
-//   passport.authenticate("google", { failureRedirect: "/" }),
-//   (req, res) => {
-//     console.log("✅ Usuario autenticado con Google:", req.user);
-//     console.log("📌 Sesión actual:", req.session);
-
-//     if (req.user) {
-//       const correo = req.user.email;
-//       const nombre = req.user.displayName;
-//       res.redirect(`bookly://login-success?correo=${encodeURIComponent(correo)}&nombre=${encodeURIComponent(nombre)}`);
-//     }
-//   }
-// );
-
-// // router.get("/googleM", (req, res, next) => {
-// //   passport.authenticate("google", {
-// //     scope: ["profile", "email"],
-// //     callbackURL: isLocal 
-// //       ? "http://10.1.65.185:3000/auth/google/callbackM"
-// //       : "https://backend-dcy8.onrender.com/auth/google/callbackM",
-// //   })(req, res, next);
-// // });
-
-
-
 module.exports = router;
-
-
-
-
-
-
-
-
-// const express = require("express");
-// const passport = require("passport");
-
-// const router = express.Router();
-
-// const isLocal = process.env.NODE_ENV !== "production";
-// const FRONTEND_URL = isLocal
-//   ? "http://localhost:8081"
-//   : "https://booklyweb-469w.onrender.com";
-
-// // Ruta para iniciar sesión con Google
-// router.get("/google", passport.authenticate("google", { scope: ["profile", "email"] }));
-
-// // Ruta de redirección después de autenticación en web
-// router.get(
-//   "/google/callback",
-//   passport.authenticate("google", { failureRedirect: "/" }),
-//   (req, res) => {
-//     console.log("✅ Usuario autenticado con Google:", req.user);
-//     console.log("📌 Sesión actual:", req.session);
-//     res.cookie("isGoogleAuth", "true", {
-//       httpOnly: false, // Necesario para acceder desde el frontend
-//       secure: process.env.NODE_ENV === "production",
-//       sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-//       maxAge: 24 * 60 * 60 * 1000, // 1 día de duración
-//     });
-//     // **Si el usuario está autenticado, guardar su correo en la cookie**
-//     if (req.user) {
-//       res.cookie("userEmail", req.user.correo, {
-//         httpOnly: false, // Necesario para acceder desde el frontend
-//         secure: process.env.NODE_ENV === "production",
-//         sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-//         maxAge: 24 * 60 * 60 * 1000, // 1 día de duración
-//       });
-//     }
-//     // Redirigir al frontend
-//     res.redirect(`${FRONTEND_URL}/inicio`);
-//   }
-// );
-
-// // // Ruta de redirección después de autenticación en móvil
-// // router.get(
-// //   "/google/callbackM",
-// //   passport.authenticate("google", { failureRedirect: "/" }),
-// //   (req, res) => {
-// //     console.log("✅ Usuario autenticado con Google:", req.user);
-// //     console.log("📌 Sesión actual:", req.session);
-
-// //     if (req.user) {
-// //       const correo = req.user.email;
-// //       const nombre = req.user.displayName;
-// //       res.redirect(`bookly://login-success?correo=${encodeURIComponent(correo)}&nombre=${encodeURIComponent(nombre)}`);
-// //     }
-// //   }
-// // );
-
-// // // router.get("/googleM", (req, res, next) => {
-// // //   passport.authenticate("google", {
-// // //     scope: ["profile", "email"],
-// // //     callbackURL: isLocal 
-// // //       ? "http://10.1.65.185:3000/auth/google/callbackM"
-// // //       : "https://backend-dcy8.onrender.com/auth/google/callbackM",
-// // //   })(req, res, next);
-// // // });
-
-
-
-// module.exports = router;
